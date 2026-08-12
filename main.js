e8d67fb5fac7fa95af98a5c8844a06371e671f29
yuliax000 <ArthyerX@gmail.com>
2026-08-12 14:49:34 +1000

    add a modal and button
    add a modal and button at the start of website

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// browser loads html page > browser loads js > open the dialog > user closes the dialog > audio system loads > user clicks sound button.
+// find the dialog
+const introDialog = document.getElementById('introDialog');
+// find intro button to close modal
+const introDialogClose = document.getElementById('introButtonClose');
+// show the found element in the browser console
+
+// console.log(introDialog);
+
+
+////// Dialog
+// open the dialog
+introDialog.showModal();
+//close the dialog
+introDialogClose.addEventListener('click', function closeIntroDialog(){
+    introDialog.close();
+});
+
+
+
 // find test button
 const testButton = document.getElementById('testButton');
 
