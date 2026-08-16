444a77bb3c7e04f83e10ac03c7fb0355257dd4a7
yuliax000 <ArthyerX@gmail.com>
2026-08-16 15:40:05 +1000

    random pitch and drum setting
    I add random pitch to each section and add drum sound after animation end to pretend it crashed

--- main.js
+++ main.js
@@ -17,7 +17,37 @@ const drum = document.getElementById('drumContainer');
 
 // create synth
 const synth = new Tone.Synth().toDestination();
+// create drum synth
+const drumSynth = new Tone.MembraneSynth().toDestination();
 
+
+
+
+// arrays for random pitch.
+const sunNotes=[ "C4", "D4", "E4", "G4", "A4",
+    "C5", "D5", "E5", "G5", "A5",
+    "C6", "E6"];
+const rainNotes = [
+    "D3", "E3", "G3", "A3", "B3",
+    "D4", "E4", "G4", "A4", "B4",
+    "D5", "E5"
+];
+const thunderNotes = [
+    "C1", "D1", "E1", "G1", "A1",
+    "C2", "D2", "E2", "G2", "A2",
+    "C3", "D3"
+];
+
+const windNotes = [
+    "A3", "B3", "C4", "D4", "E4",
+    "F4", "G4", "A4", "B4", "C5",
+    "D5", "E5", "G5"
+];
+
+const drumNotes = [
+    "C1", "C2", "D1", "D2", "E1",
+    "G1", "G2", "A1", "A2", "C3"
+];
 
 
 
@@ -44,28 +74,38 @@ introDialogClose.addEventListener('click', async function (){
 
 // add click events to weather elements
 sun.addEventListener ("click", function() {
-    synth.triggerAttackRelease("C4", "8n");
+    const note = pickRandomNote(sunNotes);
+
+    synth.triggerAttackRelease(note, "8n");
     createFallingEmoji(sun,"☀");
 });
 
 rain.addEventListener ("click", function() {
-    synth.triggerAttackRelease("E4", "8n");
+    const note = pickRandomNote(rainNotes);
+
+    synth.triggerAttackRelease(note, "8n");
     createFallingEmoji(rain, "💧");
 });
 
 thunder.addEventListener("click", function() {
-    synth.triggerAttackRelease("G2", "4n");
+    const note = pickRandomNote(thunderNotes);
+
+    synth.triggerAttackRelease(note, "4n");
     createFallingEmoji(thunder, "⚡");
 });
 
 wind.addEventListener("click", function() {
-    synth.triggerAttackRelease("A2", "2n");
+    const note = pickRandomNote(windNotes);
+
+    synth.triggerAttackRelease(note, "2n");
     createFallingEmoji(wind, "🍃");
 });
 
 drum.addEventListener("click", function (){
-   synth.triggerAttackRelease("C2", "16n");
-})
+    const note = pickRandomNote(drumNotes);
+
+   synth.triggerAttackRelease(note, "16n");
+});
 
 
 
@@ -84,9 +124,29 @@ function createFallingEmoji(container, emoji) {
 // Delete emoji when if falls off the drum
 
     fallingEmoji.addEventListener('animationend', function() {
+        playDrumSound();
         fallingEmoji.remove();
-    })
+    });
 
 }
+
+// function to pick a random note from array
+function pickRandomNote(notes) {
+   const randomIndex = Math.floor(Math.random() * notes.length);
+    return notes[randomIndex];
+}
+
+// function to play drum
+
+function playDrumSound() {
+    const note = pickRandomNote(drumNotes);
+    drumSynth.triggerAttackRelease(note,"16n");
+}
+
+
+
+
+
+
 
 
