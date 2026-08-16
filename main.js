3d7752ee9ba7956f69ab130758dca44162b10359
yuliax000 <ArthyerX@gmail.com>
2026-08-16 15:20:46 +1000

    add emoji visual effects
    I add some basic emoji falling animation to each event listener.

--- main.js
+++ main.js
@@ -45,20 +45,48 @@ introDialogClose.addEventListener('click', async function (){
 // add click events to weather elements
 sun.addEventListener ("click", function() {
     synth.triggerAttackRelease("C4", "8n");
+    createFallingEmoji(sun,"☀");
 });
 
 rain.addEventListener ("click", function() {
     synth.triggerAttackRelease("E4", "8n");
+    createFallingEmoji(rain, "💧");
 });
 
 thunder.addEventListener("click", function() {
     synth.triggerAttackRelease("G2", "4n");
+    createFallingEmoji(thunder, "⚡");
 });
 
 wind.addEventListener("click", function() {
     synth.triggerAttackRelease("A2", "2n");
+    createFallingEmoji(wind, "🍃");
 });
 
 drum.addEventListener("click", function (){
    synth.triggerAttackRelease("C2", "16n");
 })
+
+
+
+//Visual: create emoji when sections are clicked.
+function createFallingEmoji(container, emoji) {
+    const fallingEmoji = document.createElement('div');
+    fallingEmoji.classList.add('fallingEmoji');
+    fallingEmoji.textContent = emoji;
+
+    fallingEmoji.style.left = `${Math.random() * 90}%`;
+    fallingEmoji.style.fontSize = `${1+Math.random() * 2}rem`;
+
+
+    container.appendChild(fallingEmoji);
+
+// Delete emoji when if falls off the drum
+
+    fallingEmoji.addEventListener('animationend', function() {
+        fallingEmoji.remove();
+    })
+
+}
+
+
